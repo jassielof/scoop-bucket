73f6458e4a0b53dc341bdd818783aa1fe8f8e3aa
Jassiel O. <a2021117600@estudiantes.upsa.edu.bo>
2025-07-25 18:02:15 -0400

    not main

--- bin/auto-pr.ps1
+++ bin/auto-pr.ps1
@@ -1,6 +1,6 @@
 param(
     # overwrite upstream param
-    [String]$upstream = "jassielof/scoop-bucket:main"
+    [String]$upstream = "jassielof/scoop-bucket:master"
 )
 
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
